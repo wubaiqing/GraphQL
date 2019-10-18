522cfc137f8ffe2be409352e6ecbf57b63b64b89
wubaiqing (琉璃) <wubaiqing@vip.qq.com>
2019-10-18 10:13:50 +0800

    feat: 演讲演示的基础模板搭建

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,29 @@
 import "reflect-metadata";
-import { ApolloServer } from 'apollo-server-express'
-import * as Express from 'express';
-import { Resolver, Query, buildSchema } from "type-graphql";
-
+import { ApolloServer } from "apollo-server-express";
+import * as Express from "express";
+import { buildSchema, Resolver, Query } from "type-graphql";
 @Resolver()
 class HelloResolver {
   @Query(() => String)
   async hello() {
-    return "Hello!";
+    return "Hello World!";
   }
 }
 
-const main = async() => {
+const main = async () => {
   const schema = await buildSchema({
-    resolvers: [HelloResolver],
+    resolvers: [HelloResolver]
   });
 
-  const apolloServer = new ApolloServer({
-    schema,
-  })
+  const apolloServer = new ApolloServer({ schema });
 
   const app = Express();
+
   apolloServer.applyMiddleware({ app });
+
   app.listen(4000, () => {
     console.log("server started on http://localhost:4000/graphql");
-  })
-}
+  });
+};
 
 main();
